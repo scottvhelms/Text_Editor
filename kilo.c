bc4e8e5b6369963f712d42afb563befa40f8bc03
Scott Helms <scott.v.helms@gmail.com>
2021-06-09 13:53:15 -0400

    added Display keypresses

--- kilo.c
+++ kilo.c
@@ -2,6 +2,10 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+
 
 struct termios orig_termios;
 
@@ -31,7 +35,17 @@ int main()
 	enableRawMode();
 
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	{
+		if (iscntrl(c))
+		{
+			printf("%d\n", c);		
+		}
+		else
+		{
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 
 
 
