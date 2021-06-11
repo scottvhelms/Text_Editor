097cc3cc909621d05caa40603b6351aab7806e48
Scott Helms <scott.v.helms@gmail.com>
2021-06-11 14:53:32 -0400

    Refactor keyboard input

--- kilo.c
+++ kilo.c
@@ -49,6 +49,32 @@ void enableRawMode()
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+char editorReadKey()
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if(nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+/*** input ***/
+
+void editorProcessKeypress() 
+{
+	char c = editorReadKey();
+
+	switch (c) 
+	{
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
+
 /*** init ***/
 
 int main()
@@ -57,17 +83,7 @@ int main()
 
 	while (1)
 	{
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-		if (iscntrl(c))
-		{
-			printf("%d\r\n", c);		
-		}
-		else
-		{
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == CTRL_KEY('q')) break;
+		editorProcessKeypress();
 	}
 
 
