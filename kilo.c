b2cdc4763f9aa98c8a43756cafcb2c23f35ab892
Scott Helms <scott.v.helms@gmail.com>
2021-06-11 15:25:33 -0400

    Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -20,6 +20,9 @@ struct termios orig_termios;
 
 void die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -78,6 +81,8 @@ void editorProcessKeypress()
 	switch (c) 
 	{
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);		
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
 	}
