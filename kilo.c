848e862a0312ca67b1edd9f129ab48a0a5d0e6f2
Scott Helms <scott.v.helms@gmail.com>
2021-06-13 15:48:10 -0400

    ability to get terminal screen size

--- kilo.c
+++ kilo.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -15,6 +16,8 @@
 /*** data ***/
 struct editorConfig 
 {
+	int screenrows;
+	int screencols;
 	struct termios orig_termios;
 };
 
@@ -68,12 +71,31 @@ char editorReadKey()
 	return c;
 }
 
+
+int getWindowSize(int *rows, int *cols)
+{
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col ==0)
+	{
+		return -1;
+	}	
+	else
+	{
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+
+
+
+}
 /*** output ***/
 
 void editorDrawRows() 
 {
 	int y;
-	for (y=0; y<24; y++)
+	for (y=0; y<E.screenrows; y++)
 	{
 		write(STDOUT_FILENO, "~\r\n", 3);
 	}
@@ -90,7 +112,7 @@ void editorRefreshScreen()
 	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
-
+	
 /*** input ***/
 
 void editorProcessKeypress() 
@@ -110,9 +132,16 @@ void editorProcessKeypress()
 
 /*** init ***/
 
+void initEditor() 
+{
+	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
+
 int main()
 {
 	enableRawMode();
+	initEditor();
 
 	while (1)
 	{
