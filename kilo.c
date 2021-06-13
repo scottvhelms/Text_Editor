a694d452aa5b06625a3453207ceb874d2b79718f
Scott Helms <scott.v.helms@gmail.com>
2021-06-13 15:37:17 -0400

    changed global struct orig_termios to E for future use

--- kilo.c
+++ kilo.c
@@ -13,8 +13,13 @@
 
 
 /*** data ***/
+struct editorConfig 
+{
+	struct termios orig_termios;
+};
 
-struct termios orig_termios;
+struct editorConfig E;
+
 
 /*** terminal ***/
 
@@ -30,18 +35,18 @@ void die(const char *s)
 
 void disableRawMode() 
 {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 		die("tcsettattr");
 }
 
 void enableRawMode()
 {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 	
 	
 	
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
