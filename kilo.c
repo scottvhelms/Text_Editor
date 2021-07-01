fb7da072a510c57c81c51590098131b7fe865aab
Scott Helms <scott.v.helms@gmail.com>
2021-07-01 15:36:40 -0400

    limited scrolling to the right

--- kilo.c
+++ kilo.c
@@ -380,6 +380,7 @@ void editorRefreshScreen()
 
 void editorMoveCursor(int key)
 {
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 	switch (key)
 	{
 		case ARROW_LEFT:
@@ -390,10 +391,10 @@ void editorMoveCursor(int key)
 			break;
 
 		case ARROW_RIGHT:
-		
-			
+			if (row && E.cx < row->size)
+			{	
 				E.cx++;
-			
+			}
 			break;
 
 		case ARROW_UP:
