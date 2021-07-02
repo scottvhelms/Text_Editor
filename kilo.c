a25719d49cfce9b11c2861720f66ed1944f45c53
Scott Helms <scott.v.helms@gmail.com>
2021-07-01 23:47:18 -0400

    fixed the way the cursor interacts with tabs, no longer treats them as 8 spaces

--- kilo.c
+++ kilo.c
@@ -54,6 +54,7 @@ typedef struct erow
 struct editorConfig 
 {
 	int cx, cy;
+	int rx;
 	int rowoff;
 	int coloff;
 	int screenrows;
@@ -220,6 +221,21 @@ int getWindowSize(int *rows, int *cols)
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx)
+{
+	int rx = 0;
+	int j;
+	for(j = 0; j < cx; j++)
+	{
+		if (row->chars[j] == '\t')
+		{
+			rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+		}
+		rx++;
+	}
+	return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
 	int tabs = 0;
@@ -330,6 +346,12 @@ void abFree(struct abuf *ab)
 
 void editorScroll()
 {
+	E.rx = 0;
+	if (E.cy < E.numrows)
+	{
+		E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+	}	
+
 	if (E.cy < E.rowoff)
 	{
 		E.rowoff = E.cy;
@@ -337,15 +359,14 @@ void editorScroll()
 	if (E.cy >= E.rowoff + E.screenrows)
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
-		
 	}
-	if (E.cx < E.coloff)
+	if (E.rx < E.coloff)
 	{
-		E.coloff = E.cx;
+		E.coloff = E.rx;
 	}
-	if (E.cx >= E.coloff + E.screencols)
+	if (E.rx >= E.coloff + E.screencols)
 	{
-		E.coloff = E.cx - E.screencols + 1;
+		E.coloff = E.rx - E.screencols + 1;
 	}
 
 
@@ -408,7 +429,7 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 	
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 	
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -524,6 +545,7 @@ void initEditor()
 {
 	E.cx = 0;
 	E.cy = 0;
+	E.rx = 0;
 	E.rowoff = 0;
 	E.coloff = 0;
 	E.numrows = 0;
