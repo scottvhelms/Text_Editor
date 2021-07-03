5d9cff62aefc7384803f83ca58c9ef52be5cb5f6
Scott Helms <scott.v.helms@gmail.com>
2021-07-03 02:18:57 -0400

    enabled warning that then requires 3 CTRL-q to inorder to exit without saving

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -633,6 +634,7 @@ void editorMoveCursor(int key)
 
 void editorProcessKeypress() 
 {
+	static int quit_times = KILO_QUIT_TIMES;
 	int c = editorReadKey();
 
 	switch (c) 
@@ -642,6 +644,14 @@ void editorProcessKeypress()
 			break;
 
 		case CTRL_KEY('q'):
+			if (E.dirty && quit_times > 0)
+			{
+				editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+					"Press Ctrl-Q %d more times to quit.", quit_times);
+				quit_times--;
+				return;
+			}
+
 			write(STDOUT_FILENO, "\x1b[2J", 4);		
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
@@ -706,6 +716,8 @@ void editorProcessKeypress()
 			editorInsertChar(c);
 			break;
 	}
+
+	quit_times = KILO_QUIT_TIMES;
 }
 
 
