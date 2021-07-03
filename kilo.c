5732511c3ab3cb7f5858d1c3f98fcd962bff5b80
Scott Helms <scott.v.helms@gmail.com>
2021-07-03 03:37:57 -0400

    enabled the enter key for both end of line and between characters

--- kilo.c
+++ kilo.c
@@ -280,11 +280,13 @@ void editorUpdateRow(erow *row)
 	row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
-	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+	if (at < 0 || at > E.numrows) return;	
 
-	int at = E.numrows;
+	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+	memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
 	E.row[at].size = len;
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
@@ -352,11 +354,30 @@ void editorInsertChar(int c)
 {
 	if (E.cy == E.numrows)
 	{
-		editorAppendRow("", 0);
+		editorInsertRow(E.numrows, "", 0);
 	}
 	editorRowInsertChar(&E.row[E.cy], E.cx, c);
 	E.cx++;
 
+}
+
+void editorInsertNewline()
+{
+	if (E.cx == 0)
+	{
+		editorInsertRow(E.cy, "", 0);
+	}
+	else
+	{
+		erow *row = &E.row[E.cy];
+		editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+		row = &E.row[E.cy];
+		row->size = E.cx;
+		row->chars[row->size] = '\0';
+		editorUpdateRow(row);
+	}
+	E.cy++;
+	E.cx=0;
 }
 
 void editorDelChar()
@@ -425,7 +446,7 @@ void editorOpen( char *filename)
 		{
 			linelen--;		
 		}
-		editorAppendRow(line, linelen);
+		editorInsertRow(E.numrows, line, linelen);
 
 	}
 
@@ -697,7 +718,7 @@ void editorProcessKeypress()
 	switch (c) 
 	{
 		case '\r':
-			/* TODO */
+			editorInsertNewline();
 			break;
 
 		case CTRL_KEY('q'):
