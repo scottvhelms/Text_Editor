2000ad3da05077745a2bed0fb50d846743416741
Scott Helms <scott.v.helms@gmail.com>
2021-07-03 01:28:22 -0400

    enabled saving only if filename already exists(not the initial save)

--- kilo.c
+++ kilo.c
@@ -9,6 +9,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -318,6 +319,30 @@ void editorInsertChar(int c)
 
 /*** file I/O ***/
 
+char *editorRowsToString(int *buflen)
+{
+	int totlen = 0;
+	int j;
+	for(j=0; j< E.numrows; j++)
+	{
+		totlen += E.row[j].size + 1;
+	}
+	*buflen = totlen;
+
+	char *buf = malloc(totlen);
+	char *p = buf;
+	for (j=0; j<E.numrows; j++)
+	{
+		memcpy(p, E.row[j].chars, E.row[j].size);
+		p += E.row[j].size;
+		*p = '\n';
+		p++;
+	}
+
+	return buf;
+}
+
+
 void editorOpen( char *filename)
 {
 	free(E.filename);
@@ -346,6 +371,19 @@ void editorOpen( char *filename)
 }
 
 
+void editorSave()
+{
+	if (E.filename == NULL) return;
+
+	int len;
+	char *buf = editorRowsToString(&len);
+
+	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+	ftruncate(fd, len);
+	write(fd, buf, len);
+	close(fd);
+	free(buf);
+}
 
 /*** append buffer ***/
 
@@ -587,7 +625,11 @@ void editorProcessKeypress()
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
-			
+		
+		case CTRL_KEY('s'):
+			editorSave();
+			break;
+	
 		case HOME_KEY:
 			E.cx = 0;
 			break;
